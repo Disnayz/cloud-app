02763af7fb4722099e43308c6dc66717fb4655b6
Disnayz <adamdemski3@gmail.com>
2026-04-12 13:13:13 +0200

    API connecivity

--- backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03789069ab82b4b23a1df5d2c365b32704c3f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07148409ba72424f3659d630f48a65287eede74d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
